fedb7fa59eab271967633f9fc206811d3baa4070
Mariana Cardoso <mariana.cardoso@grupobarigui.com.br>
2025-03-03 14:39:07 -0300

    Correção: Adicionando supportFile para Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/", 
+    supportFile: "cypress/support/e2e.js", 
     setupNodeEvents(on, config) {
     },
   },
